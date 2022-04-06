e0447e796b37a464f23dd4a7da25d5c11aa63b63
Lester【林冠豪】 <Lester@chailease.com.tw>
2022-04-06 17:19:09 +0800

    完成排版

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,9 @@
 var time = new Date();
-let buttons = $('.continue');
-buttons.css('color','red');
+let buttons = $('.button');
+buttons.css('background','red');
 buttons.click(function (e) { 
     e.preventDefault();
     let now = new Date() - time;
     alert(now + 'ms');
     time  = new Date();
 });
-console.log(a);
