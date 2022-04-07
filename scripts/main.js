f6d01f9ba7f16eba3c96e811ebc317221ce31d47
Lester【林冠豪】 <Lester@chailease.com.tw>
2022-04-07 17:49:47 +0800

    新增按鈕事件

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,25 @@
-var time = new Date();
-let buttons = $('.button');
-buttons.css('background','red');
-buttons.click(function (e) { 
+let time = new Date();
+const buttons = $('.button');
+buttons.css('background', 'red');
+buttons.click(e => {
+    console.log(e)
     e.preventDefault();
     let now = new Date() - time;
     alert(now + 'ms');
-    time  = new Date();
 });
+
+$(document).ready(function () {
+    $('#startButton').click(function () {
+        buttons.css('background', 'white');
+        $('#startButton').attr('hidden', true);
+        setTimeout(lightUp, 5000);
+    });
+});
+
+function lightUp() {
+    time = new Date();
+}
+
+function lightDown() {
+
+}
