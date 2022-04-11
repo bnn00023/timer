155cc953b2a0ad69f1c4019af4d85cd885111cbf
Lester【林冠豪】 <Lester@chailease.com.tw>
2022-04-11 17:45:37 +0800

    新增隨機功能

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,46 @@
 let time = new Date();
 const buttons = $('.button');
+const clickTimes = [];
+
 buttons.css('background', 'red');
 buttons.click(e => {
-    console.log(e)
     e.preventDefault();
-    let now = new Date() - time;
-    alert(now + 'ms');
+    if (e.target.isLightUp) {
+        let timer = new Date() - time;
+        clickTimes.push(timer);
+        alert(timer + 'ms');
+        lightDown();
+        if (clickTimes.length == buttons.length)
+            clickTimes.push('End');
+        else
+            setTimeout(lightUp, 3000);
+    }
 });
 
 $(document).ready(function () {
     $('#startButton').click(function () {
-        buttons.css('background', 'white');
+        lightDown();
         $('#startButton').attr('hidden', true);
-        setTimeout(lightUp, 5000);
+        setTimeout(lightUp, 3000);
     });
 });
 
 function lightUp() {
+    var selectButton = buttons[getRandomInt(buttons.length)];
+    selectButton.style.background = 'red';
+    selectButton.isLightUp = true;
+    console.log(selectButton);
     time = new Date();
+
 }
 
 function lightDown() {
+    buttons.css('background', 'white');
+    $.each(buttons, function (indexInArray, valueOfElement) {
+        valueOfElement.isLightUp = false;
+    });
+}
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
